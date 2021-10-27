553ee0d2fe6eec5fc4530e35ba88ebf0fd4fb74b
perlvalkyrie <perlvalkyrie@perlvalkyrie-ideacentre-720-18ASU>
2021-10-26 20:49:39 -0500

    Edit

--- JavaPractice/src3DGameEngine/com/base/engine/MainComponent.java
+++ JavaPractice/src3DGameEngine/com/base/engine/MainComponent.java
@@ -3,6 +3,6 @@ package com.base.engine;
 public class MainComponent {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World Hi");
 	}
 }
