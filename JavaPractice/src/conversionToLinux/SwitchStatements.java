af1240f8b0df96c5609849e15b8b167e798e6fac
Randy Jenkins <jenkins-island@hotmail.com>
2021-11-04 20:19:47 -0500

    Add files via upload

---
+++ JavaPractice/src/conversionToLinux/SwitchStatements.java
@@ -0,0 +1,41 @@
+package conversionToLinux;
+
+public class SwitchStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String day = "friday";
+		
+		switch(day) {
+			case "Sunday": System.out.println("It is Sunday");
+			break;
+			case "Monday": System.out.println("It is Monday");
+			break;
+			case "Tuesday": System.out.println("It is Tuesday");
+			break;
+			case "Wednesday": System.out.println("It is Wednesday");
+			break;
+			case "Thursday": System.out.println("It is Thursday");
+			break;
+			case "Friday": System.out.println("It is Friday");
+			break;
+			case "sunday": System.out.println("It is Sunday");
+			break;
+			case "monday": System.out.println("It is Monday");
+			break;
+			case "tuesday": System.out.println("It is Tuesday");
+			break;
+			case "wednesday": System.out.println("It is Wednesday");
+			break;
+			case "thursday": System.out.println("It is Thursday");
+			break;
+			case "friday": System.out.println("It is Friday");
+			break;
+			
+			default: System.out.println("That is not a day.");
+			
+		}
+	}
+
+}
---
+++ JavaPractice/src/conversionToLinux/WrapperClass.java
@@ -0,0 +1,19 @@
+package conversionToLinux;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Randy";
+		
+		if(a==true) {
+			System.out.println("This is true");
+		}
+	}
+
+}
