ca271b21dd2948c4dfb54f186f10f19ccc4bc715
Randy Jenkins <jenkins-island@hotmail.com>
2021-11-10 19:02:35 -0600

    Python lessons

---
+++ Python/first_program/main.py
@@ -0,0 +1,10 @@
+# Learning Python Programming with CodeWithJonathon
+
+# First Variable 
+name = input("What is your name?")
+print()
+print("My name is: " + name)
+print()
+print("Yes, " + name + " is my name!")
+
+
